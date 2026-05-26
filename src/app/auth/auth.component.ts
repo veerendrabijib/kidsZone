8837e9a9cd9874e7d880625c75deaf21f1669e44
akhila-bijib <akhila@bijib.in>
2026-05-26 12:08:10 +0530

    changes

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "app-auth",
+    templateUrl: "./auth.component.html",
+    styleUrls: [],
+})
+export class AuthComponent implements OnInit {
+    ngOnInit(): void {
+        throw new Error("Method not implemented.");
+    }
+}
