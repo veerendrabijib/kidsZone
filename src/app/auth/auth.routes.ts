36501c7650cab355469d3e0eee63108f7f2cf804
akhila-bijib <akhila@bijib.in>
2026-06-01 12:13:35 +0530

    Remove node_modules from repository

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -5,9 +5,6 @@ export const authRoutes: Routes = [
         path: "", loadComponent: () => import("./login/login.component").then((c) => c.LoginComponent),
     },
     {
-        path: "forgot-password", loadComponent: () => import("./forgot-password/forgot-password.component").then((c) => c.ForgotPasswordComponent),
-    },
-    {
         path: "signup", loadComponent: () => import("./signup/signup.component").then((c) => c.SignupComponent),
     },
 ];
