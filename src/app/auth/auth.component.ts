4c874bca350ff5c21227ad263a335d68f0b4bb73
akhila-bijib <akhila@bijib.in>
2026-06-02 12:22:57 +0530

    changes

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from "@angular/core";
-import { RouterModule } from "@angular/router";
+import { Component } from "@angular/core";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: "app-auth",
   standalone: true,
+  imports: [RouterModule],
   templateUrl: "./auth.component.html"
 })
 export class AuthComponent {
--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -1,11 +1,19 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './auth.component';
 
 export const authRoutes: Routes = [
-    { 
-        path: "" ,loadComponent: () => import("./login/login.component").then((c) => c.LoginComponent) 
-        
-    },
-    {
-        path: "signup", loadComponent: () => import("./signup/signup.component").then((c) => c.SignupComponent),
-    },
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./login/login.component').then((c) => c.LoginComponent),
+      },
+      {
+        path: 'signup',
+        loadComponent: () => import('./signup/signup.component').then((c) => c.SignupComponent),
+      },
+    ],
+  },
 ];
