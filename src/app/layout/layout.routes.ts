5c928ba27783e66c27d78ec2812e9c41d77c75fe
chandra <chandrasekhar@bijib.in>
2026-06-02 15:12:56 +0530

    changes

--- src/app/layout/layout.routes.ts
+++ src/app/layout/layout.routes.ts
@@ -2,16 +2,13 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout.component';
 
 export const layoutRoutes: Routes = [
-   {
-      path: '',
-      component: LayoutComponent,
-      children: [
-         {
-            path: '', redirectTo: 'home', pathMatch: 'full'
-         },
-         {
-            path: 'home', loadComponent: () => import('./home/home.component').then((c) => c.HomeComponent)
-         }
-      ]
-   }
+ {
+  path: '',
+  component: LayoutComponent,
+  children: [
+   { path: '', redirectTo: 'home', pathMatch: 'full' },
+   { path: 'home', loadComponent: () => import('./home/home.component').then((c) => c.HomeComponent) },
+   { path: 'spinner', loadComponent: () => import('./spinner/spinner.component').then((c) => c.SpinnerComponent) }
+  ]
+ }
 ];
---
+++ src/app/layout/spinner/spinner.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+ selector: 'app-spinner',
+ standalone: true,
+ imports: [CommonModule],
+ templateUrl: './spinner.component.html',
+})
+export class SpinnerComponent {
+ rotation = 0;
+ spinning = false;
+ selectedAnimal = '';
+
+ animals = [
+  { name: 'Lion', img: 'assets/animals/lion.png', color: '#facc15' },
+  { name: 'Elephant', img: 'assets/animals/elephant.png', color: '#ec4899' },
+  { name: 'Monkey', img: 'assets/animals/monkey.png', color: '#22c55e' },
+  { name: 'Tiger', img: 'assets/animals/tiger.png', color: '#a855f7' },
+  { name: 'Rabbit', img: 'assets/animals/rabbit.png', color: '#3b82f6' },
+  { name: 'Bear', img: 'assets/animals/bear.png', color: '#f97316' },
+  { name: 'Panda', img: 'assets/animals/panda.png', color: '#14b8a6' },
+  { name: 'Cat', img: 'assets/animals/cat.png', color: '#eab308' }
+ ];
+
+ spin() {
+  if (this.spinning) return;
+  this.spinning = true;
+  const randomIndex = Math.floor(Math.random() * this.animals.length);
+  const segmentAngle = 360 / this.animals.length;
+  const stopAngle = 360 * 6 + (360 - randomIndex * segmentAngle);
+  this.rotation += stopAngle;
+  setTimeout(() => {
+   this.selectedAnimal = this.animals[randomIndex].name;
+   console.log(this.selectedAnimal)
+   this.spinning = false;
+  }, 5000);
+ }
+}
