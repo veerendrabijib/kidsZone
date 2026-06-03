b87811675ad04ad7fc923bee257d13218c0ddff2
chandra <chandrasekhar@bijib.in>
2026-06-03 12:35:03 +0530

    changes

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-splash',
-  standalone: true,
-  imports: [],
-  templateUrl: './splash.component.html'
+ selector: 'app-splash',
+ standalone: true,
+ imports: [],
+ templateUrl: './splash.component.html'
 })
 export class SplashComponent {
 
+
+ characters: any = [
+  { id: 1, name: "Dino", path: "assets/sprites/characters/dino.png" },
+  { id: 2, name: "Unicorn", path: "assets/sprites/characters/unicorn.png" },
+  { id: 3, name: "Robo", path: "assets/sprites/characters/robo.png" },
+  { id: 4, name: "Lion", path: "assets/sprites/characters/lion.png" },
+ ]
 }
