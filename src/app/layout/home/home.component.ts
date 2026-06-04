a52e2d0f8247809b57e5c8ca28f0d88f7db9317b
akhila-bijib <akhila@bijib.in>
2026-06-04 18:13:31 +0530

    home page changes

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html'
 })
 export class HomeComponent {
+    private readonly router = inject(Router)
+
     name = 'Hasvi';
-    gender="girl";
+    gender = "girl";
+
+    onStartLearningClick() {
+        // Navigate to the learning page
+        this.router.navigate(['/layout/learning']);
+    }
+
+    onExploreGamesClick() {
+        // Navigate to the games page
+        this.router.navigate(['/layout/games']);
+    }
+
+    onSpinnerClick() {
+        // Navigate to the Spinner page
+        this.router.navigate(['/layout/spinner']);
+    }
+
+    onSlamBookClick() {
+        // Navigate to the slam book page
+        this.router.navigate(['/layout/slam']);
+    }
 }
---
+++ src/app/layout/slam/slam.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slam',
+  standalone: true,
+  imports: [],
+  templateUrl: './slam.component.html'
+})
+export class SlamComponent {
+
+}
